046256efd62337b0c13d0f9272fce83e12f8bc09
Khanh <tran_khanh@outlook.com>
2021-02-03 12:27:56 +0700

    implement grid

---
+++ src/components/base/Grid.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Grid = styled.ul`
+    margin-top:
+    margin-top: -${props => props.theme.spaceS};
+    margin-left: -${props => props.theme.spaceS};
+    display: flex;
+    flex-wrap: wrap;
+    @media (min-width: 40em) {
+        margin-top: -${props => props.theme.spaceM};
+        margin-left: -${props => props.theme.spaceM};
+      }
+`
+
+export const GirdItem = styled.li`
+    padding-top: ${props => props.theme.spaceS};
+    padding-left: ${props => props.theme.spaceS};
+    flex: 0 0 50%
+    height: 14em;
+    @media(min-width: 40em){
+        padding-top: ${props => props.theme.spaceM};
+        padding-left: ${props => props.theme.spaceM};
+        flex-basis: 33%;
+    }
+    @media (min-width: 55em) {
+        flex-basis: 25%;
+      }
+    @media (min-width: 70em) {
+        flex-basis: 20%;
+      }
+    @media (min-width: 85em) {
+        flex-basis: 16.6666%;
+      }
+`
